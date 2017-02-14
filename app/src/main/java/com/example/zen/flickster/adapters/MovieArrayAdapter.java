e77f7b1238d0bd7444195716ba45d3a95f6f9a55
Zen <zuzen324@gmail.com>
2017-02-14 17:39:53 +0800

    basic movie flicks

---
+++ app/src/main/java/com/example/zen/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,57 @@
+package com.example.zen.flickster.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.zen.flickster.R;
+import com.example.zen.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Zen on 2017/2/14.
+ */
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies){
+        super(context, android.R.layout.simple_list_item_1,movies);//是什麼意思
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get the data item for position ??
+        Movie movie = getItem(position);
+
+        //check the existing view being reused ??
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        //find the image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        //clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        //populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        //return the view
+        return convertView;
+    }
+}
